badce71dfac72821856f4d95b45f96003260a2db
nolan48 <nolan19951120@gmail.com>
2024-04-26 22:44:52 +0800

    改成導入components跟scss module

--- machi-next/pages/_app.js
+++ machi-next/pages/_app.js
@@ -30,7 +30,7 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <LoaderProvider close={2} CustomLoader={CatLoader}>
-      <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider>
+      <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider> 
     </LoaderProvider>
   )
 }
