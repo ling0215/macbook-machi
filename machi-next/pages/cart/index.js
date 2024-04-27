729aa2f68ea83fcdb142f43f987e3c2a9c130174
nolan48 <nolan19951120@gmail.com>
2024-04-27 18:10:42 +0800

    重新提取

--- machi-next/pages/cart/index.js
+++ machi-next/pages/cart/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 // eslint-disable-next-line import/no-unresolved
 import CartPage1 from '@/components/cart/cart-page1'
+import CartList from '@/components/cart/list'
 export default function text1() {
   return (
-
-      <CartPage1></CartPage1>
+      <CartList></CartList>
+      // <CartPage1></CartPage1>
 
   )
 }
