e847548de068d5e3e5e6095f5da5a2c4b404f8a5
Yuan <ga523485@gmail.com>
2024-05-02 00:44:17 +0800

    article update

--- machi-next/components/blog/article-date.js
+++ machi-next/components/blog/article-date.js
@@ -4,18 +4,19 @@ import "air-datepicker/air-datepicker.css";
 import React, { useEffect, useRef } from "react";
 
 function AirDatepickerReact(props) {
-  let $input = useRef();
-  let dp = useRef();
+    let $input = useRef();
+    let dp = useRef();
 
-  useEffect(() => {
-    dp.current = new AirDatepicker($input.current, { ...props });
-  }, []);
+    useEffect(() => {
+        dp.current = new AirDatepicker($input.current, { ...props });
+    }, []);
 
-  useEffect(() => {
-    dp.current.update({ ...props });
-  }, [props]);
+    useEffect(() => {
+        dp.current.update({ ...props });
+    }, [props]);
 
-  return <input  placeholder="請選擇日期區間" ref={$input} />;
+
+    return <input placeholder="請選擇日期區間" ref={$input} />;
 }
 
 export default AirDatepickerReact;
