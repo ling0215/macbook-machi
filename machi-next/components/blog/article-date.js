be9bbf303a61c32ac6bc94a3530a2483a751d4d3
Yuan <ga523485@gmail.com>
2024-05-01 23:12:26 +0800

    add articl-date

---
+++ machi-next/components/blog/article-date.js
@@ -0,0 +1,21 @@
+import AirDatepicker from "air-datepicker";
+import "air-datepicker/air-datepicker.css";
+
+import React, { useEffect, useRef } from "react";
+
+function AirDatepickerReact(props) {
+  let $input = useRef();
+  let dp = useRef();
+
+  useEffect(() => {
+    dp.current = new AirDatepicker($input.current, { ...props });
+  }, []);
+
+  useEffect(() => {
+    dp.current.update({ ...props });
+  }, [props]);
+
+  return <input  placeholder="請選擇日期區間" ref={$input} />;
+}
+
+export default AirDatepickerReact;
