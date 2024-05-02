c67601bf658810e0d07cd76c3f4429648fe69758
rarachi <armin69969@gmail.com>
2024-05-02 16:01:44 +0800

    文章圖片上傳

--- machi-next/services/blog.js
+++ machi-next/services/blog.js
@@ -4,3 +4,23 @@ import useSWR from 'swr'
 export const publish = async (publishData = {}) => {
   return await axiosInstance.post('/blog/publish', publishData)
 }
+
+export function upload(file) {
+  return new Promise((resolve, reject) => {
+    const formData = new FormData()
+    formData.append('articleImage', file)
+    axiosInstance
+      .post('/blog/upload', formData)
+      .then((response) => {
+        console.log(response)
+        return response.data
+      })
+      .then((result) => {
+        console.log(`upload result : ${result.url}`)
+        resolve({
+          default: result.url,
+        })
+      })
+      .catch(reject)
+  })
+}
