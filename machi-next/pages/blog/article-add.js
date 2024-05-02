a9809338d9c21afb3cd1f801a292084abb5c3a86
Yuan <ga523485@gmail.com>
2024-05-02 11:54:50 +0800

    date update

---
+++ machi-next/pages/blog/article-add.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Add from '@/components/blog/article-add/article-add'
+
+export default function AddArticle() {
+  return (
+    <>
+      <h1>新增文章</h1>
+      <div>
+        <Add></Add>
+      </div>
+    </>
+  )
+}
