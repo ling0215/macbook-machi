6cbedb57de31cb2480cdd2115a9f35e188f0af9f
Lynn <lynnchang20@gmail.com>
2024-05-05 01:34:21 +0800

    update size, deco selections

--- machi-next/pages/customized-products/deco.js
+++ machi-next/pages/customized-products/deco.js
@@ -1,6 +1,8 @@
 import Steps from '@/components/customize/steps'
 import StepTitle from '@/components/customize/step-titles'
+import CakePreview from '@/components/customize/cake-preview'
 import CakeSize from '@/components/customize/cake-size'
+import DecoSelector from '@/components/customize/deco-selector'
 import styles from '@/components/customize/customize.module.css'
 
 export default function CustomizedDeco() {
@@ -8,25 +10,67 @@ export default function CustomizedDeco() {
     <>
       <div>
         <div className="lynn-custom-row">
+          <Steps stepNumber="1" stepText="選擇蛋糕尺寸" />
           <Steps
             bgStyle={styles['bg-yellow']}
             textStyle={styles['custom-text-border-yellow']}
-            stepNumber="1"
-            stepText="選擇蛋糕尺寸"
+            stepNumber="2"
+            stepText="選擇蛋糕口味及樣式"
           />
-          <Steps stepNumber="2" stepText="選擇蛋糕口味及樣式" />
           <Steps stepNumber="3" stepText="加入購物車" />
         </div>
-        <StepTitle title="Step1 : 選擇蛋糕尺寸" shouldHide1 />
-        <div className="lynn-choose-size">
-          <div className="lynn-cake4">
-            <CakeSize imageSize={100} size="4吋" price="420元" />
+        <div className="lynn-step-title">
+          <StepTitle
+            title="Step 2 : 選擇蛋糕口味及樣式"
+            prvLink="/customized-products/size"
+            nextLink="/customized-products/cart"
+          />
+        </div>
+        <div className="lynn-deco-preview">
+          <div className="lynn-preview-section">
+            <CakePreview size="6吋" />
+            <div className="lynn-preview-cake">
+              <CakeSize imageSize={170} size="預覽示意圖" price=" " />
+            </div>
           </div>
-          <div className="lynn-cake6">
-            <CakeSize imageSize={140} size="6吋" price="650元" />
-          </div>
-          <div className="lynn-cake9">
-            <CakeSize imageSize={160} size="9吋" price="1,080元" />
+          <div className="lynn-deco-selection">
+            <DecoSelector
+              decoTitle="請選擇蛋糕體層數"
+              decoSubtitle="*3層為基本，每加一層 + NT$50"
+              options={[
+                { value: 'layer3', label: '3層' },
+                { value: 'layer4', label: '4層' },
+                { value: 'layer5', label: '5層' },
+              ]}
+              displayType="radio"
+            />
+            <DecoSelector
+              decoTitle="請選擇蛋糕口味"
+              decoSubtitle=""
+              options={[
+                { value: 'earl-grey', label: '伯爵' },
+                { value: 'matcha', label: '抹茶' },
+                { value: 'choco', label: '巧克力' },
+              ]}
+              displayType="radio"
+            />
+            <div className="lynn-deco-checkbox">
+              <DecoSelector
+                decoTitle="請選擇蛋糕表面裝飾"
+                decoSubtitle="*每加一項裝飾 + NT$20"
+                options={[
+                  { value: 'strawberry', label: '草莓' },
+                  { value: 'cherry', label: '櫻桃' },
+                  { value: 'chocolate', label: '巧克力' },
+                  { value: 'macaron', label: '馬卡龍' },
+                  { value: 'orange', label: '蜜漬橙片' },
+                  { value: 'blueberry', label: '藍莓' },
+                  { value: 'photo', label: '其他：上傳圖片' },
+                ]}
+                displayType="checkbox"
+              />
+              <input type="file" id="deco-photo" name="deco-photo" accept="*" />
+            </div>
           </div>
         </div>
       </div>
