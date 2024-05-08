d5d393965a64f716c4fb6a544825d58e3c7a8324
rarachi <armin69969@gmail.com>
2024-05-08 17:26:08 +0800

    價錢篩選

--- machi-next/components/blog/latest-article.js
+++ machi-next/components/blog/latest-article.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function LatestArticles() {
   return (
     <>
-        <ul className="article-list">
-                                <li>
-                                    <div className="image-text">
-                                        <div className="image">
-                                            <img src="/images/blog/article1.jpg" alt="" />
-                                        </div>
-                                        <p>
-                                            若你是懂吃又懂拍的女孩的話，近期火紅的「Kōhī
-                                            Bar」將會是你的新地標！
-                                            採外帶式設計，但店門外有一整排的座位區，可供客人休息，下午時間陽光灑下超舒適。療癒的雪人可可不只好喝還很好拍，店內的甜點也堪稱一絕，還有號稱有甜點迷心中前三名的司康。
-                                        </p>
-                                    </div>
-                                </li>
-                            </ul>
+      <ul className="article-list">
+        <li>
+          <div className="image-text">
+            <div className="image">
+              <img src="/images/blog/article1.jpg" alt="" />
+            </div>
+            <p>
+              若你是懂吃又懂拍的女孩的話，近期火紅的「Kōhī
+              Bar」將會是你的新地標！
+              採外帶式設計，但店門外有一整排的座位區，可供客人休息，下午時間陽光灑下超舒適。療癒的雪人可可不只好喝還很好拍，店內的甜點也堪稱一絕，還有號稱有甜點迷心中前三名的司康。
+            </p>
+          </div>
+        </li>
+      </ul>
     </>
   )
 }
