ec8fc659870b09a9bbac36f726575a8148173b7c
satoshi861 <861oliver@gmail.com>
2024-05-10 05:16:13 +0800

    course調整

--- machi-next/components/course/course-list/index.js
+++ machi-next/components/course/course-list/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ProductCard from './product-card';
+import CourseCard from './course-card';
 
-export default function ProductList({ products }) {
+export default function CourseList({ products }) {
   return (
     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
       {products.map(product => (
-        <ProductCard key={product.id} product={product} name={product.name} />
+        <CourseCard key={product.id} product={product} name={product.name} />
       ))}
     </div>
   );
