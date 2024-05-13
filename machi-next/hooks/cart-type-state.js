b6688c8b69ddf7d208f0e79e9924427c1c8e5473
nolan48 <nolan19951120@gmail.com>
2024-05-13 12:43:36 +0800

    初始值搬入context

--- machi-next/hooks/cart-type-state.js
+++ machi-next/hooks/cart-type-state.js
@@ -1,52 +1,94 @@
 import React, { useState, useContext, createContext, useEffect } from 'react'
 import {
   init,
-  initState,
   addOne,
   findOneById,
   updateOne,
   removeOne,
   incrementOne,
   decrementOne,
-  generateCartState,
 } from './cart-type-state-reducer'
-import useLocalStorage from './use-localstorage'
+import { fetchCart } from '@/services/cart'
+import { useAuth } from '@/hooks/use-auth'
 
 const CartContext = createContext(null)
 
-export const CartTypeProvider = ({
-  children,
-  initialCartItems = [],
-  localStorageKey = 'cart',
-}) => {
-  let items = initialCartItems
+export const CartTypeProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([])
+  const [cartState, setCartState] = useState(init([]))
+  const [error, setError] = useState(null)
+  const { auth } = useAuth()
+  const [formattedCartItems, setFormattedCartItems] = useState([])
 
-  if (!items.length) {
-    try {
-      if (typeof window !== 'undefined') {
-        const item = window.localStorage.getItem(localStorageKey)
-        items = item ? JSON.parse(item) : []
+  useEffect(() => {
+    // 当组件加载和 auth.userData 更新时，尝试从数据库获取购物车数据
+    const loadCartData = async () => {
+      if (auth.userData && auth.userData.user_id) {
+        try {
+          const data = await fetchCart(auth.userData.user_id)
+          if (data.error) {
+            setError('获取购物车数据失败')
+          } else {
+            setFormattedCartItems(data || [])
+            setCartState(init(data || []))
+          }
+        } catch (error) {
+          setError('加载购物车数据失败')
+          console.error(error)
+        }
       }
-    } catch (error) {
-      items = []
-      console.log(error)
     }
-  }
 
-  const [cartItems, setCartItems] = useState(items)
-  const [cartState, setCartState] = useState(init(initialCartItems))
-  const [storedValue, setValue] = useLocalStorage(localStorageKey, items)
+    loadCartData()
+  }, [auth.userData])
+  console.log(formattedCartItems)
+  console.log(cartState)
+  useEffect(() => {
+    // 确保 formattedCartItems.items 存在并且是一个数组
+    if (formattedCartItems.items && Array.isArray(formattedCartItems.items)) {
+      const newFormattedCartItems = formattedCartItems.items
+        .map((item) => {
+          if (item.product_id_fk) {
+            return {
+              id: item.cart_item_id,
+              quantity: item.product_count,
+              price: item.product_price,
+              name: item.product_name,
+              image: '',
+              type: 'product',
+            }
+          } else if (item.class_id_fk) {
+            return {
+              id: item.cart_item_id,
+              quantity: item.class_count,
+              price: item.class_price,
+              name: item.class_name,
+              image: '',
+              type: 'class',
+              classtime: '2024/08/10',
+              address: '復興堡',
+            }
+          } else if (item.custom_id_fk) {
+            return {
+              id: item.cart_item_id,
+              quantity: item.custom_count,
+              price: item.custom_price,
+              name: item.custom_name,
+              image: '',
+              type: 'custom',
+            }
+          }
+          return null
+        })
+        .filter((item) => item !== null)
+      setCartItems(newFormattedCartItems)
+    }
+  }, [formattedCartItems]) // 正确设置依赖项
+  console.log(cartItems)
 
   useEffect(() => {
-    setCartItems(initialCartItems) // 更新 cartItems 以匹配最新的 initialCartItems
-    setCartState(init(initialCartItems)) // 重新初始化 cartState
-  }, [initialCartItems])
-
-  useEffect(() => {
-    if (JSON.stringify(cartItems) !== storedValue) {
-      setValue(cartItems)
-    }
-    setCartState(generateCartState(cartState, cartItems))
+    // 更新 cartState 以匹配最新的 cartItems
+    setCartState(init(cartItems))
   }, [cartItems])
 
   const addItem = (item) => {
@@ -82,14 +124,13 @@ export const CartTypeProvider = ({
 
   const decrement = (id, type) => {
     setCartItems(decrementOne(cartItems, id, type))
-    console.log(type)
   }
 
   return (
     <CartContext.Provider
       value={{
         cart: cartState,
-        items: cartState.items,
+        items: cartItems,
         addItem,
         removeItem,
         updateItem,
