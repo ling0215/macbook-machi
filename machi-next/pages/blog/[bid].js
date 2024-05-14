ef9edd21726237f90837cdac1674944b79642deb
Yuan <ga523485@gmail.com>
2024-05-14 16:23:58 +0800

    update

--- machi-next/pages/blog/[bid].js
+++ machi-next/pages/blog/[bid].js
@@ -1,14 +1,14 @@
-import Article from '@/components/blog/article-detail/article-detail'
+import Adetail from '@/components/blog/article-detail/article-detail'
 import styles from '@/styles/blog/article-detail.module.scss'
 import { useRouter } from 'next/router'
 export default function ArticleDetail() {
   const router = useRouter()
-  const  id  = router.query.bid // 从路由参数中获取文章 ID
-  console.log({ id })
+  // const id = router.query.bid // 从路由参数中获取文章 ID
+  // console.log({ id })
   return (
     <>
-      <Article articleId={router.query.bid} />
-      <div className={`container ${styles['message-container']}`}>
+      <Adetail articleId={router.query.bid} />
+      <div className={`container`}>
         <div className={styles['message-area']}>
           <h4>留言區</h4>
           <p>我要留言</p>
