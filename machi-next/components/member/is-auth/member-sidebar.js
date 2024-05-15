1b68193f81424ab7710396a9736efadd627503c2
Ling <cindy09919@gmail.com>
2024-05-16 03:51:10 +0800

    homepagr/member update

--- machi-next/components/member/is-auth/member-sidebar.js
+++ machi-next/components/member/is-auth/member-sidebar.js
@@ -4,22 +4,22 @@ import styles from '../member.module.scss';
 
 function MemberSidebar() {
   return (
-    <div className={`${styles['menu']} d-none d-lg-block`}>
+    <div className={`${styles['menu']} d-none d-lg-block col-lg-5 me-lg-5`}>
       <ul className='d-flex flex-column list-unstyled'>
         <li className='p-2'>
-          <Link href="/member/profile" className={`${styles['member-sidebar']} p-2`} >會員資料</Link>
+          <Link href="/member/account" className={`${styles['member-sidebar']} p-2`} >會員資料</Link>
         </li>
         <li className='p-2'>
-          <Link href="/member/order-query" className={`${styles['member-sidebar']} p-2`} >訂單查詢</Link>
+          <Link href="/member/account/order-query" className={`${styles['member-sidebar']} p-2`} >訂單查詢</Link>
         </li>
         <li className='p-2'>
-          <Link href="/member/favorite-products" className={`${styles['member-sidebar']} p-2`} >收藏商品</Link>
+          <Link href="/member/account/favorite-products" className={`${styles['member-sidebar']} p-2`} >收藏商品</Link>
         </li>
         <li className='p-2'>
-          <Link href="/member/favorite-articles" className={`${styles['member-sidebar']} p-2 `} >收藏文章</Link>
+          <Link href="/member/account/favorite-articles" className={`${styles['member-sidebar']} p-2 `} >收藏文章</Link>
         </li>
         <li className='p-2'>
-          <Link href="/member/coupons" className={`${styles['member-sidebar']} p-2`} >專屬優惠券</Link>
+          <Link href="/member/account/coupons" className={`${styles['member-sidebar']} p-2`} >專屬優惠券</Link>
         </li>
       </ul>
     </div>
